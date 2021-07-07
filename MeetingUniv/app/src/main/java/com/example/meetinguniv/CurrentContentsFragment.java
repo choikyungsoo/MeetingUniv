5510de3b1e83a35a0185c5c0d0e91f11268f8588
choikyungsoo <81173010+choikyungsoo@users.noreply.github.com>
2021-07-08 03:26:06 +0900

    현황 RecyclerView 구현
    탭 레이아웃 왔다갔다시 반복생성되는 문제 해결 필요

--- MeetingUniv/app/src/main/java/com/example/meetinguniv/CurrentContentsFragment.java
+++ MeetingUniv/app/src/main/java/com/example/meetinguniv/CurrentContentsFragment.java
@@ -3,62 +3,51 @@ package com.example.meetinguniv;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link CurrentContentsFragment#newInstance} factory method to
+ * Use the  factory method to
  * create an instance of this fragment.
  */
 public class CurrentContentsFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public CurrentContentsFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment CurrentContentsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static CurrentContentsFragment newInstance(String param1, String param2) {
-        CurrentContentsFragment fragment = new CurrentContentsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
+    private ArrayList<CurrentRecycleritem> list = new ArrayList<CurrentRecycleritem>();
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        RecyclerView recyclerView = view.findViewById(R.id.recycler1) ;
+        CurrentAdapterRecyclerItem recyclerItemAdapter = new CurrentAdapterRecyclerItem(this.list);
+        recyclerView.setAdapter(recyclerItemAdapter) ;
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        addRecyclerItem("~님 외 * 명 + ~ 님 외 * 명", "성공", "2020/07/14 05:10");
+        recyclerItemAdapter.notifyDataSetChanged();
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_current_contents, container, false);
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_current_contents, container, false);
+        return rootView;
+    }
+
+    private void addRecyclerItem(String description, String check, String data){
+        CurrentRecycleritem recyclerItem = new CurrentRecycleritem();
+        recyclerItem.setDescription(description);
+        recyclerItem.setCheck(check);
+        recyclerItem.setDate(data);
+        list.add(recyclerItem);
+
     }
 }
--- MeetingUniv/app/src/main/java/com/example/meetinguniv/MatchingContentsFragment.java
+++ MeetingUniv/app/src/main/java/com/example/meetinguniv/MatchingContentsFragment.java
@@ -1,64 +1,50 @@
 package com.example.meetinguniv;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link MatchingContentsFragment#newInstance} factory method to
+ * Use the  factory method to
  * create an instance of this fragment.
  */
 public class MatchingContentsFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public MatchingContentsFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MatchingContentsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MatchingContentsFragment newInstance(String param1, String param2) {
-        MatchingContentsFragment fragment = new MatchingContentsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private View view;
+    private Button collectBtn;
+    private Button matchingBtn;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_matching_contents, container, false);
+        view = inflater.inflate(R.layout.fragment_matching_contents, container, false);
+
+        collectBtn =  (Button)view.findViewById(R.id.teambtn);
+        matchingBtn = (Button)view.findViewById(R.id.matchbtn);
+
+        collectBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        //팝업창 구현이 필요한 버튼
+        matchingBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        return view;
     }
 }
