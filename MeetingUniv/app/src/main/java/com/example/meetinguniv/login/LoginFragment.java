581d9a8d275e4a5be7757ad9854dc77d98ab3b64
choikyungsoo <81173010+choikyungsoo@users.noreply.github.com>
2021-07-17 00:57:37 +0900

    아이디 찾기 추가 및 연결

--- MeetingUniv/app/src/main/java/com/example/meetinguniv/login/LoginFragment.java
+++ MeetingUniv/app/src/main/java/com/example/meetinguniv/login/LoginFragment.java
@@ -17,6 +17,8 @@ public class LoginFragment extends Fragment {
 
     private Button login_btn;
     private TextView join_btn;
+    private TextView findingID_btn;
+    private TextView findingPW_btn;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -25,6 +27,10 @@ public class LoginFragment extends Fragment {
 
     public void onViewCreated(View view, Bundle savedInstanceState) {
         this.login_btn = view.findViewById(R.id.login_btn);
+        this.join_btn = view.findViewById(R.id.joinBTN);
+        this.findingID_btn = view.findViewById(R.id.FindingIDBTN);
+        this.findingPW_btn = view.findViewById(R.id.FindingPWBTN);
+
         this.login_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -32,11 +38,25 @@ public class LoginFragment extends Fragment {
             }
         });
 
-        this.join_btn = view.findViewById(R.id.joinBTN);
+
         this.join_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Navigation.findNavController(view).navigate(R.id.action_login_to_join_profile);
+            }
+        });
+
+        this.findingID_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(view).navigate(R.id.action_login_to_findIDScreenFragment);
+            }
+        });
+
+        this.findingPW_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(view).navigate(R.id.action_login_to_findingPWScreenFragment);
             }
         });
     }
