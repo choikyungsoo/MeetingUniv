2bbee37ddc838fb5a0bdc408944fdd949c000e0b
choikyungsoo <81173010+choikyungsoo@users.noreply.github.com>
2021-07-23 16:35:00 +0900

    친구목록 완성

---
+++ MeetingUniv/app/src/main/java/com/example/meetinguniv/main/FriendsListRecycleritem.java
@@ -0,0 +1,23 @@
+package com.example.meetinguniv.main;
+
+public class FriendsListRecycleritem {
+    private int F_chatingProfile;
+    private String F_memberlist;
+
+    public int getF_chatingProfile() {
+        return F_chatingProfile;
+    }
+
+    public void setF_chatingProfile(int f_chatingProfile) {
+        F_chatingProfile = f_chatingProfile;
+    }
+
+    public String getF_memberlist() {
+        return F_memberlist;
+    }
+
+    public void setF_memberlist(String f_memberlist) {
+        F_memberlist = f_memberlist;
+    }
+
+}
