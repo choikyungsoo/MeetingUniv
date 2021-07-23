db0356cb02725c24d964f16a356b46eac121fccd
MoonInbae <mib010213@gmail.com>
2021-07-23 19:40:17 +0900

    20210723 Code Review again
    20210723 Code Review again

--- MeetingUniv/app/src/main/java/com/example/meetinguniv/login/LoginActivity.java
+++ MeetingUniv/app/src/main/java/com/example/meetinguniv/login/LoginActivity.java
@@ -5,8 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.meetinguniv.R;
-import com.pedro.library.AutoPermissions;
-import com.pedro.library.AutoPermissionsListener;
 
 public class LoginActivity extends AppCompatActivity {
 
