52a375a8eced70beb4186871bf486c8262a8ea05
MoonInbae <mib010213@gmail.com>
2021-08-24 07:15:19 +0900

    카카오 초기화
    카카오 초기화

---
+++ MeetingUniv/app/src/main/java/com/softcon/meetinguniv/GlobalApplication.java
@@ -0,0 +1,17 @@
+package com.softcon.meetinguniv;
+
+import android.app.Application;
+import android.util.Log;
+
+import com.kakao.sdk.common.KakaoSdk;
+
+public class GlobalApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Kakao Sdk 초기화
+        KakaoSdk.init(this, "e73b9463cb4d98b9d5460cc48a8fabb0");
+//        Log.i("INITS", "Kakao Sdk 초기화");
+    }
+}
