3931ffa4bffcd28e108b65a79cc8a437ed6ef1f2
MoonInbae <mib010213@gmail.com>
2021-08-28 19:05:46 +0900

    카카오 로그인 작업중
    카카오 로그인 작업중

--- MeetingUniv/app/src/main/java/com/softcon/meetinguniv/login/KakaoLoginFragment.java
+++ MeetingUniv/app/src/main/java/com/softcon/meetinguniv/login/KakaoLoginFragment.java
@@ -2,24 +2,27 @@ package com.softcon.meetinguniv.login;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
+import com.kakao.sdk.user.UserApiClient;
+import com.kakao.sdk.user.model.Account;
 import com.softcon.meetinguniv.R;
 import com.softcon.meetinguniv.main.MainActivity;
 
 public class KakaoLoginFragment extends Fragment {
 
-    private Button login_btn;
-    private TextView join_btn;
-    private TextView findingID_btn;
-    private TextView findingPW_btn;
+    private static final String TAG = "유저";
+    private ImageView kakao_login_btn;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -28,39 +31,39 @@ public class KakaoLoginFragment extends Fragment {
 
     public void onViewCreated(View view, Bundle savedInstanceState) {
 
-        this.login_btn = view.findViewById(R.id.login_btn);
-        this.join_btn = view.findViewById(R.id.joinBTN);
-        this.findingID_btn = view.findViewById(R.id.FindingIDBTN);
-        this.findingPW_btn = view.findViewById(R.id.FindingPWBTN);
+        this.kakao_login_btn = view.findViewById(R.id.kakao_login_btn);
 
-        this.login_btn.setOnClickListener(new View.OnClickListener() {
+        this.kakao_login_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getActivity(), MainActivity.class);
-                startActivity(intent);
-                getActivity().finish();
-            }
-        });
+                UserApiClient.getInstance().loginWithKakaoTalk(this,(oAuthToken, error) -> {
+                    if(error != null){
+                        Log.e(TAG, "로그인 실패", error);
+                    }
+                    else if(oAuthToken != null){
+                        Log.i(TAG, "로그인 성공 ${token.accessToken}");
 
+                        //사용자 정보 요청
+                        UserApiClient.getInstance().me((user, meError) -> {
+                            if(meError != null){
+                                Log.e(TAG, "사용자 정보 요청 실패", meError);
+                            }
+                            else{
+                                System.out.println("로그인됐다.");
+                                Log.e(TAG, user.toString());
 
-        this.join_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Navigation.findNavController(view).navigate(R.id.action_login_to_joinAgreementScreenFragment);
-            }
-        });
+                                Account user1 = user.getKakaoAccount();
+                                System.out.println("유저 어카운트"+user1);
+                            }
+                            return null;
+                        });
 
-        this.findingID_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Navigation.findNavController(view).navigate(R.id.action_login_to_findIDScreenFragment);
-            }
-        });
-
-        this.findingPW_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Navigation.findNavController(view).navigate(R.id.action_login_to_findingPWScreenFragment);
+                    }
+                    return null;
+                });
+//                Intent intent = new Intent(getActivity(), MainActivity.class);
+//                startActivity(intent);
+//                getActivity().finish();
             }
         });
     }
