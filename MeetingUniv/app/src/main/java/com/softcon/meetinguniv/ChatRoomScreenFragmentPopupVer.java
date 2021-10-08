d033d57911380583f31ad7916dfd82145b5bc498
chaeyun8392 <81141945+chaeyun8392@users.noreply.github.com>
2021-10-08 13:23:22 +0900

    채팅방
    채팅방

--- MeetingUniv/app/src/main/java/com/softcon/meetinguniv/ChatRoomScreenFragmentPopupVer.java
+++ MeetingUniv/app/src/main/java/com/softcon/meetinguniv/ChatRoomScreenFragmentPopupVer.java
@@ -31,17 +31,21 @@ public class ChatRoomScreenFragmentPopupVer extends Fragment implements View.OnC
     private View rootView;
 
     @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        getActivity().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
         return inflater.inflate(R.layout.fragment_chat_room_screen_popup_ver, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        getActivity().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
-
         this.chatRoomMembers = view.findViewById(R.id.chatRoomMembers);
         this.numOfChatRoomMembers = view.findViewById(R.id.join_selectAreaText);
         this.chatRoomEditText = view.findViewById(R.id.join_selectUnivText);
@@ -50,45 +54,6 @@ public class ChatRoomScreenFragmentPopupVer extends Fragment implements View.OnC
 
         this.chatRoomMembers.setOnClickListener(this);
         this.chatRoomEditText.setOnClickListener(this);
-        caculatekeyboard(view);
-    }
-
-    private void caculatekeyboard(View view) {
-        Rect rectangle = new Rect();
-        view.getWindowVisibleDisplayFrame(rectangle);
-        int screenHeight = view.getRootView().getHeight();
-        int tempkeyboardsize = screenHeight - rectangle.bottom;
-
-        if (tempkeyboardsize > screenHeight * 0.1) {
-//            대부분의 키보드 높이가 전체의 10프로이상차지해서 0.1로 정함
-            keyboardheight  = screenHeight - rectangle.bottom;
-            if (!isKeyboardShowing) {
-//                키보드가 보여지는 경우
-                isKeyboardShowing = true;
-                Toast.makeText(getContext(), "키보드가 보여짐", Toast.LENGTH_SHORT).show();
-
-                DisplayMetrics metrics = getResources().getDisplayMetrics();
-                ConstraintLayout.LayoutParams params = new ConstraintLayout.LayoutParams(120,140);
-                this.ChatRoomScreen.setLayoutParams(params);
-
-            }
-        }
-        else {
-            // 키보드가 안보이는 경우
-            if (isKeyboardShowing) {
-                isKeyboardShowing = false;
-                Toast.makeText(getContext(), "키보드가 닫힘", Toast.LENGTH_SHORT).show();
-            }
-        }
-
-        this.rootView = getActivity().getWindow().getDecorView();
-        this.rootView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
-            @Override
-            public void onGlobalLayout() {
-
-                caculatekeyboard(rootView);
-            }
-        });
 
     }
 
