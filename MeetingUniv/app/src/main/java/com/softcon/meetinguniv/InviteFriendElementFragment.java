3b079e3f67ece5bdd1268e4c86d5ed0b3f0b9558
chaeyun8392 <81141945+chaeyun8392@users.noreply.github.com>
2021-11-09 01:29:47 +0900

    채팅방 메뉴(친구초대, 나가기)
    채팅방 메뉴(친구초대, 나가기)

--- MeetingUniv/app/src/main/java/com/softcon/meetinguniv/InviteFriendElementFragment.java
+++ MeetingUniv/app/src/main/java/com/softcon/meetinguniv/InviteFriendElementFragment.java
@@ -9,12 +9,14 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import java.util.ArrayList;
 
-public class InviteFriendElementFragment extends Fragment {
-    private ArrayList<EditTeamRecycleritem1> alllist = new ArrayList<EditTeamRecycleritem1>();
-    private RecyclerView allfriends;
+public class InviteFriendElementFragment extends Fragment implements View.OnClickListener {
+    private ArrayList<EditTeamRecycleritem> currentlist = new ArrayList<EditTeamRecycleritem>();
+    private RecyclerView currentfriends;
+    private Button inviteFriendBack_BTN;
 
     private EditTeamAdapterRecycleritem recyclerItemAdapter;
 
@@ -23,16 +25,43 @@ public class InviteFriendElementFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_invite_friend_element, container, false);
+        this.inviteFriendBack_BTN = view.findViewById(R.id.inviteFriendBack_BTN);
+        this.inviteFriendBack_BTN.setOnClickListener(this);
 
         //리사이클러뷰 - 현재 팀원   /////////////////////////////////////////////////////////////
-        this.allfriends = view.findViewById(R.id.inviteFriendsList);
-        this.recyclerItemAdapter = new EditTeamAdapterRecycleritem(this.alllist);
-        this.allfriends.setAdapter(recyclerItemAdapter) ;
+        this.currentfriends = view.findViewById(R.id.inviteFriendsList);
+        EditTeamAdapterRecycleritem2 recyclerItemAdapter2 = new EditTeamAdapterRecycleritem2(this.currentlist);
+        this.currentfriends.setAdapter(recyclerItemAdapter2) ;
 
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
-        linearLayoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
-        this.allfriends.setLayoutManager(linearLayoutManager);
+        LinearLayoutManager linearLayoutManager2 = new LinearLayoutManager(getContext());
+        this.currentfriends.setLayoutManager(linearLayoutManager2);
+
+        //DB연결 시 addRecyclerItem2를 통해 친구목록 가져오기
+
+        addRecyclerItem2(R.drawable.prot, "친구 1");
+        addRecyclerItem2(R.drawable.prot2, "친구 2");
+        addRecyclerItem2(R.drawable.prot3, "친구 3");
 
         return view;
+    }
+
+    //모든 친구 목록
+    private void addRecyclerItem2(int profile, String memberlist){
+        EditTeamRecycleritem recyclerItem = new EditTeamRecycleritem();
+        recyclerItem.setE_memporife(profile);
+        recyclerItem.setE_memname(memberlist);
+        this.currentlist.add(recyclerItem);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.inviteFriendBack_BTN:
+                getActivity().getSupportFragmentManager().beginTransaction()
+                        .replace(this, this)
+                        .commit();
+                imm.hideSoftInputFromWindow(addTeamSearch.getWindowToken(),0);
+                break;
+        }
     }
 }
