51fea2cf383542c38d58d8020707f55b3fc0d180
chaeyun8392 <81141945+chaeyun8392@users.noreply.github.com>
2021-11-22 23:51:19 +0900

    1:1채팅 가기
    1:1채팅 가기

--- MeetingUniv/app/src/main/java/com/softcon/meetinguniv/main/FriendsProfileScreenFragment.java
+++ MeetingUniv/app/src/main/java/com/softcon/meetinguniv/main/FriendsProfileScreenFragment.java
@@ -16,7 +16,7 @@ import com.softcon.meetinguniv.R;
 public class FriendsProfileScreenFragment extends Fragment implements View.OnClickListener{
 
     private Button F_bBTN;
-
+    private Button goToFriendChatRoom_BTN;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -24,7 +24,9 @@ public class FriendsProfileScreenFragment extends Fragment implements View.OnCli
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_friends_profile_screen, container, false);
         this.F_bBTN = view.findViewById(R.id.F_backBTN);
+        this.goToFriendChatRoom_BTN = view.findViewById(R.id.goToFriendChatRoom_BTN);
         this.F_bBTN.setOnClickListener(this);
+        this.goToFriendChatRoom_BTN.setOnClickListener(this);
         return view;
     }
 
@@ -34,10 +36,17 @@ public class FriendsProfileScreenFragment extends Fragment implements View.OnCli
             case R.id.F_backBTN:
                 backtoFriends(v);
                 break;
+            case R.id.goToFriendChatRoom_BTN:
+                goToFriendChatRoom(v);
+                break;
         }
     }
 
     private void backtoFriends(View v) {
         Navigation.findNavController(v).navigate(R.id.action_friendsProfileScreenFragment_to_mainFragment);
+    }
+
+    private void goToFriendChatRoom(View v) {
+        Navigation.findNavController(v).navigate(R.id.action_friendsProfileScreenFragment_to_chatRoomScreenFragmentPopupVer);
     }
 }
