75c07b1bddfc42e7e5e78fd7d121e7ca4330c823
MoonInbae <mib010213@gmail.com>
2022-02-04 11:13:16 +0900

    20220204 Code Review
    20220204 Code Review
    Intro 키보드 현상 해결

--- MeetingUniv/app/src/main/java/com/softcon/meetinguniv/main/MainActivity.java
+++ MeetingUniv/app/src/main/java/com/softcon/meetinguniv/main/MainActivity.java
@@ -8,6 +8,8 @@ import androidx.navigation.Navigation;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Space;
 import android.widget.Toast;
 
@@ -28,6 +30,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+//        inputMethodManager.hideSoftInputFromWindow(Context.getWindowToken(), 0);
+
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+
         this.statusBarHeight = 0;
         int resId = getResources().getIdentifier("status_bar_height", "dimen", "android");
         if(resId>0){
