18db220b795f1b795959dda2ade5cfef7d87f63e
chaeyun8392 <81141945+chaeyun8392@users.noreply.github.com>
2022-02-07 12:59:45 +0900

    edit team 수정
    edit team 수정

--- MeetingUniv/app/src/main/java/com/softcon/meetinguniv/main/MatchingContentFragment.java
+++ MeetingUniv/app/src/main/java/com/softcon/meetinguniv/main/MatchingContentFragment.java
@@ -39,6 +39,8 @@ import org.xmlpull.v1.XmlPullParserFactory;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.URL;
 import java.util.ArrayList;
