74d20675006e8ec6df1df6e3ccf8285850883d59
chaeyun8392 <81141945+chaeyun8392@users.noreply.github.com>
2022-02-22 11:11:03 +0900

    recycleritem_chatmembers 수정
    recycleritem_chatmembers 수정

--- MeetingUniv/app/src/main/java/com/softcon/meetinguniv/main/MatchingChatingRecycleritem.java
+++ MeetingUniv/app/src/main/java/com/softcon/meetinguniv/main/MatchingChatingRecycleritem.java
@@ -1,17 +1,19 @@
 package com.softcon.meetinguniv.main;
 
 public class MatchingChatingRecycleritem {
-    private int chatingProfile;
+//    private int chatingProfile;
     private String memberlist;
     private String dDay;
+    private String chatMemberNum;
+    private String newMessageNum;
 
-    public int getChatingProfile() {
-        return chatingProfile;
-    }
-
-    public void setChatingProfile(int chatingProfile) {
-        this.chatingProfile = chatingProfile;
-    }
+//    public int getChatingProfile() {
+//        return chatingProfile;
+//    }
+//
+//    public void setChatingProfile(int chatingProfile) {
+//        this.chatingProfile = chatingProfile;
+//    }
 
     public String getMemberlist() {
         return memberlist;
@@ -27,5 +29,21 @@ public class MatchingChatingRecycleritem {
 
     public void setDDay(String dDay) {
         this.dDay = dDay;
+    }
+
+    public String getChatMemberNum() {
+        return chatMemberNum;
+    }
+
+    public void setChatMemberNum(String chatMemberNum) {
+        this.chatMemberNum = chatMemberNum;
+    }
+
+    public String getNewMessageNum() {
+        return newMessageNum;
+    }
+
+    public void setNewMessageNum(String newMessageNum) {
+        this.newMessageNum = newMessageNum;
     }
 }
